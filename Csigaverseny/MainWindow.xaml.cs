73d0e531b883e3d817c222f393bac2684c07f86a
Kristóf_B <jkacsa2004@gmail.com>
2022-01-09 14:01:18 +0100

    csiga (gombok nem működnek még teljesen, javítandó)

--- Csigaverseny/MainWindow.xaml.cs
+++ Csigaverseny/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Threading;
 
 namespace Csigaverseny
 {
@@ -20,9 +22,66 @@ namespace Csigaverseny
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer idozites;
+        int csigaszam1 = 21;
+        int csigaszam2 = 21;
+        int csigaszam3 = 21;
+
         public MainWindow()
         {
             InitializeComponent();
+            idozites = new DispatcherTimer();
+            idozites.Interval = TimeSpan.FromSeconds(0.3);
+#pragma warning disable CS8622
+            idozites.Tick += new EventHandler(Mozgascs);
+#pragma warning restore CS8622
+            ujfutam.IsEnabled = false;
+        }
+
+        private void Mozgascs(object sender, EventArgs e)
+        {
+            idozites.Start();
+            start.IsEnabled = false;
+            ujbajnoksag.IsEnabled = false;
+            Random rnd = new Random();
+            csigaszam1 += rnd.Next(20, 100);
+            csiga1.Margin = new Thickness(csigaszam1, 78, 0, 0);
+            if (csigaszam1 > 779)
+            {
+                csiga1.Margin = new Thickness(779, 78, 0, 0);
+            }
+            
+            Random rnd2 = new Random();
+            csigaszam2 += rnd2.Next(20, 100);
+            csiga2.Margin = new Thickness(csigaszam2, 267, 0, 0);
+            if (csigaszam2 > 779)
+            {
+                csiga2.Margin = new Thickness(779, 267, 0, 0);
+            }
+            
+            Random rnd3 = new Random();
+            csigaszam3 += rnd3.Next(20, 100);
+            csiga3.Margin = new Thickness(csigaszam3, 452, 0, 0);
+            if (csigaszam3 > 810)
+            {
+                csiga3.Margin = new Thickness(779, 452, 0, 0);
+            }
+
+            if (csiga1.Margin == new Thickness(779, 78, 0, 0) && csiga2.Margin == new Thickness(779, 267, 0, 0) && csiga3.Margin == new Thickness(810, 452, 0, 0))
+            {
+                idozites.Stop();
+                ujfutam.IsEnabled = true;
+                ujbajnoksag.IsEnabled = true;
+            }
+        }
+
+        private void Ujfutam(object sender, RoutedEventArgs e)
+        {
+            csiga1.Margin = new Thickness(21, 78, 0, 0);
+            csiga2.Margin = new Thickness(21, 267, 0, 0);
+            csiga3.Margin = new Thickness(21, 452, 0, 0);
+            start.IsEnabled = true;
+            ujfutam.IsEnabled = false;
         }
     }
 }
